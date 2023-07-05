e7199abee7706b75c8caa713983d2219393a6f48
Christian Castillo <castillochristian3@gmail.com>
2023-07-05 11:21:57 -0400

    created second background and styled

---
+++ client/src/components/BackGround2.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../styles/BackGround2Styles.css';
+
+
+function BackGround2() {
+  return (
+    <div className='background-2'>
+        <div className='heading'>
+          <h1>Heading</h1>
+          <p>Text</p>  
+        </div>
+    </div>
+  )
+}
+
+export default BackGround2
--- client/src/routes/Projects.js
+++ client/src/routes/Projects.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import NavBar from '../components/NavBar';
+import BackGround2 from '../components/BackGround2';
+import Footer from '../components/Footer';
 
 function Projects() {
   return (
     <div>
         <NavBar />
+        <BackGround2 />
+        <Footer />
     </div>
   )
 }
