eec84a2bffecff8012daf7088c554c24f3c45505
Christian Castillo <castillochristian3@gmail.com>
2023-07-05 09:40:20 -0400

    created navbar

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,52 @@
+import { Link } from "react-router-dom";
+import { FaBars, FaTimes} from "react-icons/fa";
+import "../styles/NavBarStyle.css";
+import React, { useState } from 'react';
+
+function NavBar() {
+
+    const [display, setDisplay] = useState(false);
+    const [color, setColor] = useState(false);
+
+    const handleClick = () => {
+        setDisplay(!display);
+    }
+
+    const changeColor = () => {
+        if (window.scrollY >= 100) {
+            setColor(true);
+        } else {
+            setColor(false);
+        }
+    }
+
+    window.addEventListener("scroll", changeColor);
+
+    return (
+        <div className={ color ? "header header-bg" : "header" }>
+            <Link to="/">
+                <h1>Portfolio</h1>
+            </Link>
+            <ul className={display ? "nav-menu active" : "nav-menu"}>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/projects">Projects</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+            </ul>
+            <div className="hamburger" onClick={ handleClick }>
+                {display ? (
+                    <FaTimes size={ 20 } style={{ color: "white" }} />
+                ) : (
+                    <FaBars size={ 20 } style={{ color: "white" }} />
+                )}     
+            </div>
+        </div>
+    )
+}
+
+export default NavBar
