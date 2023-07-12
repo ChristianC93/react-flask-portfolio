8a080bb5e87767650b873e15a1dde2ae7fcd3bae
Christian Castillo <castillochristian3@gmail.com>
2023-07-12 09:18:46 -0400

    created link for github icon in footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { FaHome, FaLinkedin, FaMailBulk } from 'react-icons/fa';
+import { Link } from 'react-router-dom'
+import { FaLinkedin, FaMailBulk } from 'react-icons/fa';
 import { GrGithub } from 'react-icons/gr';
 import '../styles/FooterStyles.css';
 
@@ -19,7 +20,7 @@ function Footer() {
                 <h4>About Me</h4>
                 <p>A software developer, lifelong learner, new yorker, and big fan of FromSoftware.</p>
                 <div className='socials'>
-                    <GrGithub size={ 30 } style={{ color: 'white', marginRight: "1rem" }} />
+                    <Link to='https://github.com/ChristianC93'><GrGithub size={ 30 } style={{ color: 'white', marginRight: "1rem" }} /></Link> 
                     <FaLinkedin size={ 30 } style={{ color: 'white', marginRight: "1rem" }} />
                 </div>
             </div>
